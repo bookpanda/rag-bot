7dad108a3c1f3024a046e60ca256214c91ca2dc3
Idhibhat Pankam <marginpankam@gmail.com>
2024-09-01 22:58:57 +0700

    prompt config

---
+++ src/config/promptConfig.ts
@@ -0,0 +1,29 @@
+import fs from "fs";
+import path from "path";
+import { z } from "zod";
+
+interface PromptConfig {
+  translate: {
+    prompt: string;
+    rules: string[];
+  };
+}
+
+const readJSONFile = <T>(filename: string): T => {
+  const currentDir = process.cwd();
+  const filePath = path.join(currentDir, "src/config", filename);
+  const rawData = fs.readFileSync(filePath, "utf-8");
+  const jsonData = JSON.parse(rawData);
+  return jsonData;
+};
+const rawPromptconfig: PromptConfig =
+  readJSONFile<PromptConfig>("prompt_config.json");
+
+const promptConfigSchema = z.object({
+  translate: z.object({
+    prompt: z.string(),
+    rule: z.array(z.string()),
+  }),
+});
+
+export const promptConfig = promptConfigSchema.parse(rawPromptconfig);
